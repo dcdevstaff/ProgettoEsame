3f03b5d78baf12690bb1a4159ca4dddf06811046
dcdevstaff <dcdevstaff@gmail.com>
2018-03-27 19:07:37 +0200

    Danilo
    eliminato exit()

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -18,26 +18,26 @@ if (isset($_POST['submit'])){
 	//Check for empty fields
 	if (empty($first) || empty($last) || empty($email) || empty($password) || empty($scadenza)) {
 		header("Location: ../signup.php?signup=empty");
-		exit();
+		
 
 	}else {
 		
 		//Check if input are valid
 		if (!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$/", $last) ) {
 			header("Location: ../signup.php?signup=invalid");
-			exit();
+			
 		}else{
 			//Check Mail Valid
 			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 				header("Location: ../signup.php?signup=email");
-				exit();
+				
 			}else{
 				$sql=" SELECT *  FROM users WhERE user_email = '$email'  ;";
 				$result = mysqli_query($conn,$sql);
 				$resultCheck = mysqli_num_rows($result);
 				if ($resultCheck>0) {
 					header("Location: ../signup.php?signup=usertaken");
-					exit();
+					
 				}else{
 					//Hashing the password
 					$hashedPwd = password_hash($password,PASSWORD_DEFAULT);
@@ -52,7 +52,6 @@ if (isset($_POST['submit'])){
 					 window.location.href='../HomeIOT.php';
 					 </script>");
 					
-					exit();
 				}
 			}
 		}
@@ -67,7 +66,7 @@ if (isset($_POST['submit'])){
 
 }else{
 	header("Location: ../signup.php?errore"); 
-	exit();
+	
 }
 
 ?>
