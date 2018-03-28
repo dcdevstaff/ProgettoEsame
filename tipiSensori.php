fa8834a4e01699050167b1f41d273342b4249345
dcdevstaff <dcdevstaff@gmail.com>
2018-03-28 17:17:38 +0200

    Aggiunta Pagina tipiSensori

---
+++ tipiSensori.php
@@ -0,0 +1,88 @@
+<?php
+
+
+include_once 'includes/dbh.inc.php';
+include_once 'header.php';
+
+?>
+
+
+<section class="cover cover--single" style="margin-top: 50px">
+		<div class="cover__filter"></div>
+		<div class="cover__caption">
+			<div class="cover__caption__copy">
+				<h1 class="centrto">PANNELLO DI CONTROLLO IOT - Tipi Sensori </h1>
+			</div>
+		</div>
+	</section>
+
+	<section class="cards clearfix">
+				<h1 class="evidenzia">Gestione dati dei Sesori</h1>
+
+<?php
+if (!isset($_POST['submitSens'])){
+?>	
+
+<div><!-- Button trigger modal -->
+	<form class="signup-form" action="tipiSensori.php" method="POST" >
+
+	<h3>Tipo</h3><input type="text" name="tipo" placeholder="Tipo di Sensore">
+	<h3>Numero Campi</h3><input type="text" name="nCampi" placeholder="Numero dei campi">
+<button type="submit" name="submitSens" >Crea Struttura Sensore</button>
+</form>
+</div>
+</section>
+<?php 
+}else{
+	$num=$_POST['nCampi'];
+	?>
+	<div class="card">
+			<img class="card__image"src="https://source.unsplash.com/category/nature/400x260" alt="Nature">
+			<div class="card__copy">
+				<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#ModalSensor">Aggiuni i Campi</button>
+				<p>Verranno creati <?php echo $num ?> campi</p>
+			</div>
+		</div>
+	</section>
+<?php
+}
+
+?>
+
+
+<!-- MODAL ADD ACCOUNT-->
+<section>
+<div class="modal fade" id="ModalSensor" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Aggiungi Info al Tipo di Sensore</h4>
+      </div>
+      <div class="modal-body">
+	  <form class="signup-form" action="includes/tipiSensori.inc.php" method="POST" >
+	  	<?php 
+	  	for( $i=1;$i<=$num;$i++){
+		?>
+	  		<h3>Campo <?php echo $i ?></h3><input type="text" name=<?php echo 'campo'.$i ?> placeholder="Inserisci nome campo">
+	  	<?php 
+	  }
+	  	 ?>
+
+	<button type="submit" name="submit" >Aggiungi i seguenti campi</button>
+	</form>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+
+
+<?php
+	include_once 'footer.php';
+?>
+
